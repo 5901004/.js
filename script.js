aad0da149b5d4dacc265c0a2b964c5832e9828d8
5901004 <166626719+5901004@users.noreply.github.com>
2024-04-18 17:45:24 -0400

    .js

---
+++ script.js
@@ -0,0 +1,15 @@
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+if (num1<num2 && num3<num2){
+    console.log(num2)
+    }
+       
+if(num1>num3 && num1>num2 ){
+        console.log(num1)
+    }
+
+if(num3>num1 && num3>num2 ){
+        console.log(num3)
+    }
